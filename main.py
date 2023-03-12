6650d3ef33a8e7fcefa9340d542fd44975ffc920
Victor Felipe <ds.victorfelipe@gmail.com>
2023-03-12 18:26:12 -0300

    Criado funcao de exibicao de mensagem

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ def gether_data():
 
     return n1, n2
 
-def main():
-    n1, n2 = gether_data()
-
-    print(n1**n2)
+def print_message(n1, n2):
+    print(f'Os valores {n1} e {n2} somados dão {n1+n2}')
 
     return None
 
+
+def main():
+    n1, n2 = gether_data()
+
+    print_message(n1, n2)
+
+    return None
 
 if __name__ == "__main__":
     main()
